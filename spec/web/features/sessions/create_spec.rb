c5f7b14d21647fc8b66a6dc54a80a268cd5841fa
nickgnd <nick.gnd@gmail.com>
2017-02-23 14:03:29 +0100

    FIX features spec for POST /sessions route

--- spec/web/features/sessions/create_spec.rb
+++ spec/web/features/sessions/create_spec.rb
@@ -14,27 +14,37 @@ describe 'POST /sessions api' do
   end
 
   it 'responds with 201 with the right credentials' do
-    post '/sessions', { user: { email: 'test@email.com', password: 'secret' }}
+    header 'Accept', 'application/json'
+    header 'Content-Type', 'application/json'
+    post '/sessions', JSON.generate({ user: { email: 'test@email.com', password: 'secret' }})
     assert_equal 201, last_response.status
   end
 
   it 'responds with auth_token with the right credentials' do
-    post '/sessions', { user: { email: 'test@email.com', password: 'secret' }}
+    header 'Accept', 'application/json'
+    header 'Content-Type', 'application/json'
+    post '/sessions', JSON.generate({ user: { email: 'test@email.com', password: 'secret' }})
     refute_nil JSON.parse(last_response.body).fetch('auth_token', nil)
   end
 
   it 'responds with 401 status with wrong email' do
-    post '/sessions', { user: { email: 'wrong_email@email.com', password: 'secret' }}
+    header 'Accept', 'application/json'
+    header 'Content-Type', 'application/json'
+    post '/sessions', JSON.generate({ user: { email: 'wrong_email@email.com', password: 'secret' }})
     assert_equal 401, last_response.status
   end
 
   it 'responds with 401 status with wrong password' do
-    post '/sessions', { user: { email: 'test@email.com', password: 'wrong_password' }}
+    header 'Accept', 'application/json'
+    header 'Content-Type', 'application/json'
+    post '/sessions', JSON.generate({ user: { email: 'test@email.com', password: 'wrong_password' }})
     assert_equal 401, last_response.status
   end
 
   it 'responds with 401 without required params' do
-    post '/sessions', { user: { password: 'secret' }}
+    header 'Accept', 'application/json'
+    header 'Content-Type', 'application/json'
+    post '/sessions', JSON.generate({ user: { password: 'secret' }})
     assert_equal 401, last_response.status
   end
 end
